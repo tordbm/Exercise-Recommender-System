5114791f9ecf6e6da2efc606a2a12f7a2d95914c
tordbm <tordbm@icloud.com>
2023-10-22 14:27:17 +0200

    fix indexes

--- rand_exercises.py
+++ rand_exercises.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import random as rand
 
-df = pd.read_csv("100_exercises.csv", sep=";")
-
+df = pd.read_csv("100_exercises.csv", sep=";", header=None)
 df.columns = ['Index', 'Title']
 
 exercises = set()
-
 while len(exercises) < 20:
     random = rand.randint(0, 119)
-    exercises.add(df.iloc[random]['Title'])
-
+    exercises.add(df.loc[random]['Title'])
+    
 for i in exercises:
     print(i)
     
